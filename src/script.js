a82111230c37b5dd0dc09feb2cdbebb10b75154e
mjepis7 <meiwashita@usp.br>
2026-09-22 15:02:12 -0300

    feat: set up night lighting with door point light

--- src/script.js
+++ src/script.js
@@ -150,6 +150,7 @@ house.add(walls)
 const roof = new THREE.Mesh(
     new THREE.ConeGeometry(3.5, 1.5, 4),
     new THREE.MeshStandardMaterial({
+        color: '#29180c',
         map: roofColorTexture,
         aoMap: roofARMTexture,
         roughnessMap: roofARMTexture,
@@ -249,13 +250,18 @@ for (let i = 0; i < 30; i++) {
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+const ambientLight = new THREE.AmbientLight('#86cdff', 0.275)
 scene.add(ambientLight)
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight('#ffffff', 1.5)
+const directionalLight = new THREE.DirectionalLight('#86cdff', 1)
 directionalLight.position.set(3, 2, -8)
 scene.add(directionalLight)
+
+// Door light
+const doorLight = new THREE.PointLight('#ff5c46', 5)
+doorLight.position.set(0, 2.2, 2.5)
+house.add(doorLight)
 
 /**
  * Sizes
