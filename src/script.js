e60d0b26f4ec78b0d148a66c5d63353f2b503aab
mjepis7 <meiwashita@usp.br>
2026-09-22 16:24:18 -0300

    feat: add exponential fog matching the twilight sky

--- src/script.js
+++ src/script.js
@@ -370,6 +370,11 @@ sky.material.uniforms.mieDirectionalG.value = 0.95
 sky.material.uniforms.sunPosition.value.set(0.3, -0.038, -0.95)
 
 /**
+ * Fog
+ */
+scene.fog = new THREE.FogExp2('#04343f', 0.08)
+
+/**
  * Animate
  */
 const timer = new Timer()
